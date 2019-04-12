cf4b8fbabb036c5f2c7a4d79d23536fc4c42a67c
Geeks Empire <Support@GeeksEmpire.net>
2019-04-12 02:34:31 -0800

    [Time] 04-12-2019 | 02:30
    [Description] Floating Shortcuts | Quickly Switch Between Apps by Floating Shortcuts | Multitasking Master 📱⌚️
    
    [Phone][ChangeLog]
    ~ Initialized Floating Widgets with Configuration Activity
    ~ Optimizing Applications Opening Process
    ~ Clearify In-App Strings | Floating Popup Menu
    ~ Optimizing Orientation (Landscape/Portrait) Change Handler
    ~ Optimizing Broadcast Receiver in Floating Services
    ~ Split Activities Excluded from Recent Apps Menu
    ~ Optimizing Sign in Process
    ~ Fixed (WARNING: API 'variant.getExternalNativeBuildTasks()' is obsolete and has been replaced with 'variant.getExternalNativeBuildProviders()')
    ~ UI Enhancement | Android 9 (Pie) & Higher
    ~ Float It
    - Open All Apps in Floating View (FreeForm)
    ~ Theme of Icons (Package of Icons)
    ~ Lite Preferences
    - An Option to Set Preferences to Minimum for Better Performance (Highly Recommended for Mid-Range & Low-End Smartphones)
    
    [Watch][ChangeLog]
    ~ Optimizing Applications Opening Process
    ~ Clearify In-App Strings | Floating Popup Menu
    
    [Build][ChangeLog]
    ~ Add NullpointerException in LicenseValidator signature.verify() for License Library

--- FloatingShortcutsPhone/build/generated/source/buildConfig/betaConfig/debug/net/geekstools/floatshort/PRO/BuildConfig.java
+++ FloatingShortcutsPhone/build/generated/source/buildConfig/betaConfig/debug/net/geekstools/floatshort/PRO/BuildConfig.java
@@ -8,6 +8,6 @@ public final class BuildConfig {
   public static final String APPLICATION_ID = "net.geekstools.floatshort.PRO";
   public static final String BUILD_TYPE = "debug";
   public static final String FLAVOR = "betaConfig";
-  public static final int VERSION_CODE = 404;
-  public static final String VERSION_NAME = "14.000.000.404_PRO[BETA]";
+  public static final int VERSION_CODE = 406;
+  public static final String VERSION_NAME = "14.000.000.406_PRO[BETA]";
 }
