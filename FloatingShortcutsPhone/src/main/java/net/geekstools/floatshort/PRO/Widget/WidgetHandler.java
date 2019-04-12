6e795dc650b63eff15d99925b55a4d7661ad2b0a
Geeks Empire <Support@GeeksEmpire.net>
2019-04-11 23:26:51 -0800

    [Time] 04-11-2019 | 23:30
    [Description] Floating Shortcuts | Quickly Switch Between Apps by Floating Shortcuts | Multitasking Master 📱⌚️
    
    [Phone][ChangeLog]
    ~ Initialized Floating Widgets with Configuration Activity
    ~ Optimizing Applications Opening Process
    ~ Clearify In-App Strings | Floating Popup Menu
    ~ Optimizing Orientation (Landscape/Portrait) Change Handler
    ~ Optimizing Broadcast Receiver in Floating Services
    ~ Split Activities Excluded from Recent Apps Menu
    ~ Optimizing Sign in Process
    ~ Fixed (WARNING: API 'variant.getExternalNativeBuildTasks()' is obsolete and has been replaced with 'variant.getExternalNativeBuildProviders()')
    ~ UI Enhancement | Android 9 (Pie) & Higher
    ~ Float It
    - Open All Apps in Floating View (FreeForm)
    ~ Theme of Icons (Package of Icons)
    ~ Lite Preferences
    - An Option to Set Preferences to Minimum for Better Performance (Highly Recommended for Mid-Range & Low-End Smartphones)
    
    [Watch][ChangeLog]
    ~ Optimizing Applications Opening Process
    ~ Clearify In-App Strings | Floating Popup Menu
    
    [Build][ChangeLog]
    ~ Add NullpointerException in LicenseValidator signature.verify() for License Library

--- FloatingShortcutsPhone/src/main/java/net/geekstools/floatshort/PRO/Widget/WidgetHandler.java
+++ FloatingShortcutsPhone/src/main/java/net/geekstools/floatshort/PRO/Widget/WidgetHandler.java
@@ -3,8 +3,10 @@ package net.geekstools.floatshort.PRO.Widget;
 import android.app.Activity;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProviderInfo;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
+import android.view.View;
 import android.widget.ImageView;
 
 import net.geekstools.floatshort.PRO.R;
@@ -23,21 +25,41 @@ public class WidgetHandler extends Activity {
 
         functionsClass = new FunctionsClass(getApplicationContext(), WidgetHandler.this);
 
-        AppWidgetManager manager = AppWidgetManager.getInstance(this);
-        List<AppWidgetProviderInfo> widgetProviderInfoList = manager.getInstalledProviders();
+        AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(this);
+        List<AppWidgetProviderInfo> widgetProviderInfoList = appWidgetManager.getInstalledProviders();
 
+        int widgetIndex = 0;
         for (AppWidgetProviderInfo appWidgetProviderInfo : widgetProviderInfoList) {
 
 
-            System.out.println(">>>>> " + appWidgetProviderInfo.label + " | " + appWidgetProviderInfo.provider + " <<<<<");
+            System.out.println(widgetIndex + " >>>>> " + appWidgetProviderInfo.configure + " | " + appWidgetProviderInfo.provider + " <<<<<");
 
-
+            widgetIndex++;
         }
 
 
         ImageView widgetPreview = (ImageView) findViewById(R.id.widgetPreview);
         try {
-            widgetPreview.setImageDrawable(widgetProviderInfoList.get(widgetProviderInfoList.size() - 1).loadPreviewImage(getApplicationContext(), DisplayMetrics.DENSITY_MEDIUM));
+            //com.google.android.keep/com.google.android.apps.keep.ui.homescreenwidget.WidgetConfigureActivity
+            widgetPreview.setImageDrawable(widgetProviderInfoList.get(21).loadPreviewImage(getApplicationContext(), DisplayMetrics.DENSITY_HIGH));
+            widgetPreview.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+
+                    Intent intent = new Intent();
+                    intent.setAction("android.appwidget.action.APPWIDGET_CONFIGURE");
+                    intent.setComponent(widgetProviderInfoList.get(21).configure);
+                    intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, widgetProviderInfoList.get(21).autoAdvanceViewId);
+
+//                    intent.setPackage("com.google.android.keep");
+                    /*intent.setClassName(*//*widgetProviderInfoList.get(17).configure.getPackageName()*//*"com.google.android.keep",
+                     *//*widgetProviderInfoList.get(17).configure.getClassName()*//*"com.google.android.apps.keep.ui.homescreenwidget.WidgetConfigureActivity");*/
+                    intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                    startActivity(intent);
+
+                }
+            });
+
         } catch (Exception e) {
             e.printStackTrace();
         }
