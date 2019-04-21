2ac3c1c65be116a4ab69511411ec7bf1563ce2f2
Geeks Empire <Support@GeeksEmpire.net>
2019-04-21 06:44:33 -0800

    [Time] 04-21-2019 | 06:30
    [Description] Floating Shortcuts | Quickly Switch Between Apps by Floating Shortcuts | Multitasking Master 📱⌚️
    
    [Phone][ChangeLog]
    ~ New UI/UX Initialized
    - Hybrid: 100%
    - List:
    - Grid:
    - Category:
    - Widgets:
    - Automation:
    ~ In-App Purchase Process Completed
    ~ Floating Widgets | βeta
    - Added Support for Light/Dark Theme
    - Added As In-App Product to Purchase
    - Save Position/Size Of Floating Widgets
    - Floating Widgets Resizing Process Completed
    - Create Floating View Of Configured Widgets
    - Process Of Saving/Loading Widgets Data Completed | Using ROOM Database
    - Customized Widget Picker
    - Launch Configuration Activity to Set Widget
    - Loading All Installed Widgets Information with Preview Image
    ~ Optimizing Applications Opening Process
    ~ Clarify In-App Strings | Floating Popup Menu
    ~ Optimizing Orientation (Landscape/Portrait) Change Handler
    ~ Optimizing Broadcast Receiver in Floating Services
    ~ Split Activities Excluded from Recent Apps Menu
    
    [Watch][ChangeLog]
    ~ Optimizing Applications Opening Process
    ~ Clarify In-App Strings | Floating Popup Menu
    
    [Build][ChangeLog]
    ~

--- FloatingShortcutsPhone/src/main/java/net/geekstools/floatshort/PRO/Widget/RoomDatabase/WidgetDataInterface.kt
+++ FloatingShortcutsPhone/src/main/java/net/geekstools/floatshort/PRO/Widget/RoomDatabase/WidgetDataInterface.kt
@@ -3,7 +3,7 @@ package net.geeksempire.chat.vicinity.Util.RoomSqLiteDatabase.UserInformation
 import androidx.room.Database
 import androidx.room.RoomDatabase
 
-@Database(entities = [WidgetDataModel::class], version = 1)
+@Database(entities = [WidgetDataModel::class], version = 1, exportSchema = false)
 abstract class WidgetDataInterface : RoomDatabase() {
     abstract fun initDataAccessObject(): WidgetDataDAO
 }
