8e3024951c99ec56754914300eba04f8f53c73f3
Geeks Empire <Support@GeeksEmpire.net>
2019-04-28 14:26:07 -0800

    [Time] 04-28-2019 | 14:30
    [Description] Floating Shortcuts | Quickly Switch Between Apps by Floating Shortcuts | Multitasking Master 📱⌚️
    
    [Phone][ChangeLog]
    ~ Graphical UI Optimization * 11
    ~ Floating Widgets Demo Section Added Into IAP Activity
    ~ Optimizing Remote Recovery of Floating Shortcuts/Categories/Widgets
    ~ Floating Widgets
    - Minimum Width/Height Removed
    - Sort Installed/Configured Widgets
    - Completed Process of Recovering Floating Widgets
    - Completed Process of Widget Label Renaming
    - Option to Create Home Screen Shortcuts Of Widgets with Layered Icon of Widget Preview & App Icon (Create Floating Widgets from Home Screen)
    - Option to Add Configured Widgets to Recovery
    - Option to Delete Configured Widgets
    - Option for System Widgets Picker
    - Added Support for Transparent Theme
    - Added Support for Light/Dark Theme
    - Added As In-App Product to Purchase
    - Save Position/Size Of Floating Widgets
    - Floating Widgets Resizing Process Completed
    - Create Floating View Of Configured Widgets
    - Process Of Saving/Loading Widgets Data Completed | Using ROOM Database
    - Customized Widget Picker
    - Launch Configuration Activity to Set Widget
    - Loading All Installed Widgets Information with Preview Image
    ~ Fixed Bug in Process of CategoryName that Added to Recovery 🔁
    ~ Optimized Navigation Process
    ~ New UI/UX Implementation Completed
    ~ In-App Purchase Process Completed
    ~ Optimizing Applications Opening Process
    ~ Clarify In-App Strings | Floating Popup Menu
    ~ Optimizing Orientation (Landscape/Portrait) Change Handler
    ~ Optimizing Broadcast Receiver in Floating Services
    ~ Split Activities Excluded from Recent Apps Menu
    
    [Watch][ChangeLog]
    ~ Optimizing Applications Opening Process
    ~ Clarify In-App Strings | Floating Popup Menu
    
    [Build][ChangeLog]
    ~

--- FloatingShortcutsPhone/build/generated/source/buildConfig/betaConfig/debug/net/geekstools/floatshort/PRO/BuildConfig.java
+++ FloatingShortcutsPhone/build/generated/source/buildConfig/betaConfig/debug/net/geekstools/floatshort/PRO/BuildConfig.java
@@ -8,6 +8,6 @@ public final class BuildConfig {
   public static final String APPLICATION_ID = "net.geekstools.floatshort.PRO";
   public static final String BUILD_TYPE = "debug";
   public static final String FLAVOR = "betaConfig";
-  public static final int VERSION_CODE = 418;
-  public static final String VERSION_NAME = "14.000.000.418_PRO[BETA]";
+  public static final int VERSION_CODE = 420;
+  public static final String VERSION_NAME = "14.000.000.420_PRO[BETA]";
 }
