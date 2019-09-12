6e74ff5f6a7fbe4124e79ce9913f110874f7dce8
GeeksEmpireNet <Support@GeeksEmpire.net>
2019-09-11 18:03:27 -0800

    [Time] 11-Sep-2019 | 18:00
    [Description] Floating Shortcuts | #FloatIt | Quickly Switch Apps | Multitasking Master 📱 ⌚
    
    [Phone][ChangeLog]
    # Firebase Cloud Functions Added
    # Initializing In-App Update
    # Floating Widgets | Checkpoint Added for Uninstalled Widgets
    # Floating Widgets | Home Screen Shortcuts Action & Category Changed
    # Security Services
    ~ Authentication Added for Floating Widgets | Configurations Activity
    ~ Authentication Added for Floating Widgets | Home Screen Shortcuts
    ~ Authentication Added for Floating Notification Dots
    ~ Authentication Added for Floating Folders | Split It
    ~ Authentication Added for Floating Shortcuts | Split It
    ~ Authentication Added for Floating Splash
    ~ Pin Password Completed
    ~ Setting Up Finger Print Authentication
    ~ Lock/Unlock Process for Apps List Added with Fingerprint
    ~ Lock/Unlock Process for Folders List Added with Fingerprint
    ~ Lock/Unlock Process for Apps List Added with Pin Password
    ~ Lock/Unlock Process for Folders List Added with Pin Password
    - Upload/Download .LockedApps File
    - 'Forgot Password' Process Completed
    - Editing/Changing Pin Password Completed
    - Rounded Corner for Custom Dialogue Views Added
    - (En/De)crypt Functions Added for Password
    
    [Watch][ChangeLog]
    #
    
    [Build][ChangeLog]
    # Libraries Updated

--- FloatingShortcutsPhone/build/generated/source/buildConfig/betaConfig/debug/net/geekstools/floatshort/PRO/BuildConfig.java
+++ FloatingShortcutsPhone/build/generated/source/buildConfig/betaConfig/debug/net/geekstools/floatshort/PRO/BuildConfig.java
@@ -8,6 +8,6 @@ public final class BuildConfig {
   public static final String APPLICATION_ID = "net.geekstools.floatshort.PRO";
   public static final String BUILD_TYPE = "debug";
   public static final String FLAVOR = "betaConfig";
-  public static final int VERSION_CODE = 650;
-  public static final String VERSION_NAME = "14.800.500.65_PRO[BETA]";
+  public static final int VERSION_CODE = 652;
+  public static final String VERSION_NAME = "14.800.500.652_PRO[BETA]";
 }
