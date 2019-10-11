a4c87ef0db2f857d12a08c873b15950785b95cc8
GeeksEmpireNet <Support@GeeksEmpire.net>
2019-10-11 03:36:15 -0800

    [Time] 11-Oct-2019 | 03:35
    [Description] #FloatIt | Quickly Switch Apps | Multitasking Master
    
    [Phone][ChangeLog]
    # New Versioning Format
    
    [Watch][ChangeLog]
    # New Versioning Format
    
    [Build][ChangeLog]
    # Libraries Updated

--- FloatingShortcutsPhone/build/generated/source/buildConfig/betaConfig/debug/net/geekstools/floatshort/PRO/BuildConfig.java
+++ FloatingShortcutsPhone/build/generated/source/buildConfig/betaConfig/debug/net/geekstools/floatshort/PRO/BuildConfig.java
@@ -8,6 +8,6 @@ public final class BuildConfig {
   public static final String APPLICATION_ID = "net.geekstools.floatshort.PRO";
   public static final String BUILD_TYPE = "debug";
   public static final String FLAVOR = "betaConfig";
-  public static final int VERSION_CODE = 672;
-  public static final String VERSION_NAME = "16.310.000.672_PRO[BETA]";
+  public static final int VERSION_CODE = 674;
+  public static final String VERSION_NAME = "[Android][Phone]5|10.11.2019{674}PRO[BETA]";
 }
