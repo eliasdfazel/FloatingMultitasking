02f6315f24299823c05700a038fd6182f3683cb3
GeeksEmpireNet <Support@GeeksEmpire.net>
2020-01-13 10:06:49 -0900

    [Time] 13-Jan-2020 | 10:00
    [Description]
    
    [Phone][ChangeLog]
    # Optimizing Functionality & UI with Kotlin & androidX Libraries
    # Refactoring Deprecated API & Functions
    
    [Watch][ChangeLog]
    #
    
    [Build][ChangeLog]
    # Libraries Updated

--- FloatItPhone/src/main/java/net/geekstools/floatshort/PRO/Util/RemoteTask/Create/FloatingWidgetHomeScreenShortcuts.kt
+++ FloatItPhone/src/main/java/net/geekstools/floatshort/PRO/Util/RemoteTask/Create/FloatingWidgetHomeScreenShortcuts.kt
@@ -1,8 +1,8 @@
 /*
  * Copyright © 2020 By Geeks Empire.
  *
- * Created by Elias Fazel on 1/13/20 9:58 AM
- * Last modified 1/13/20 9:54 AM
+ * Created by Elias Fazel on 1/13/20 10:06 AM
+ * Last modified 1/13/20 9:58 AM
  *
  * Licensed Under MIT License.
  * https://opensource.org/licenses/MIT
