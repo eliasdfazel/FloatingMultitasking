71aee706f94f50df99c699dba9689674e7e0560b
GeeksEmpireNet <Support@GeeksEmpire.net>
2020-02-21 20:03:15 -0900

    [Time] 21-Feb-2020 | 20:00
    [Description]
    # Optimizing Functionality & UI with Kotlin & androidX Libraries
    # Refactoring Deprecated API & Functions
    
    [Phone][ChangeLog]
    # Security Services Support Added for FreeForm
    # Refactor Packages
    
    [Watch][ChangeLog]
    # Refactor Packages
    
    [Build][ChangeLog]
    # Libraries Updated

--- FloatItWatch/build/generated/source/buildConfig/betaConfig/debug/net/geekstools/floatshort/PRO/BuildConfig.java
+++ FloatItWatch/build/generated/source/buildConfig/betaConfig/debug/net/geekstools/floatshort/PRO/BuildConfig.java
@@ -1,8 +1,8 @@
 /*
  * Copyright © 2020 By Geeks Empire.
  *
- * Created by Elias Fazel on 1/14/20 12:14 PM
- * Last modified 1/14/20 11:56 AM
+ * Created by Elias Fazel on 2/21/20 8:03 PM
+ * Last modified 2/21/20 5:13 PM
  *
  * Licensed Under MIT License.
  * https://opensource.org/licenses/MIT
@@ -19,5 +19,5 @@ public final class BuildConfig {
   public static final String BUILD_TYPE = "debug";
   public static final String FLAVOR = "betaConfig";
   public static final int VERSION_CODE = 707;
-  public static final String VERSION_NAME = "7.01.14.2020.707[PRO][WearOS][Watch][BETA]";
+  public static final String VERSION_NAME = "7.02.21.2020.707[PRO][WearOS][Watch][BETA]";
 }
