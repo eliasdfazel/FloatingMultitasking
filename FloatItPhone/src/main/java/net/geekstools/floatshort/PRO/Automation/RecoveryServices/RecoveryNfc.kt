3953540cf99db5a6d745bad682bc89e3382b49fc
GeeksEmpireNet <Support@GeeksEmpire.net>
2020-04-21 10:33:38 -0800

    [Time] 21-Apr-2020 | 10:35
    [Description]
    # Optimizing Functionality & UI with Kotlin & androidX Libraries
    # Refactoring Deprecated API & Functions
    
    [Phone][ChangeLog]
    1. Floating Folders Adapters Converted to Kotlin & Duplicated Processes Removed
    
    [Watch][ChangeLog]
    
    [Build][ChangeLog]
    # Libraries Updated

--- FloatItPhone/src/main/java/net/geekstools/floatshort/PRO/Automation/RecoveryServices/RecoveryNfc.kt
+++ FloatItPhone/src/main/java/net/geekstools/floatshort/PRO/Automation/RecoveryServices/RecoveryNfc.kt
@@ -2,7 +2,7 @@
  * Copyright © 2020 By Geeks Empire.
  *
  * Created by Elias Fazel
- * Last modified 4/21/20 9:40 AM
+ * Last modified 4/21/20 10:33 AM
  *
  * Licensed Under MIT License.
  * https://opensource.org/licenses/MIT
