721111779e499a2722863cdd16b2b99d095fa2d2
GeeksEmpireNet <Support@GeeksEmpire.net>
2020-04-25 05:12:27 -0800

    [Time] 25-Apr-2020 | 05:15
    [Description]
    # Optimizing Functionality & UI with Kotlin & androidX Libraries
    # Refactoring Deprecated API & Functions
    
    [Phone][ChangeLog]
    
    [Watch][ChangeLog]
    
    [Build][ChangeLog]
    # Libraries Updated

--- FloatItPhone/src/main/java/net/geekstools/floatshort/PRO/Utils/RemoteProcess/CloudMessageHandler.kt
+++ FloatItPhone/src/main/java/net/geekstools/floatshort/PRO/Utils/RemoteProcess/CloudMessageHandler.kt
@@ -2,7 +2,7 @@
  * Copyright © 2020 By Geeks Empire.
  *
  * Created by Elias Fazel
- * Last modified 4/24/20 12:36 PM
+ * Last modified 4/25/20 4:33 AM
  *
  * Licensed Under MIT License.
  * https://opensource.org/licenses/MIT
@@ -15,8 +15,9 @@ import com.google.firebase.inappmessaging.model.InAppMessage
 
 class CloudMessageHandler {
 
-    fun extractData(inAppMessage: InAppMessage, action: Action) {
-        //            val linkToGif = intent.data!!.getQueryParameter(Intent.EXTRA_STREAM).toString()
-        //            val additionalText = intent.data!!.getQueryParameter(Intent.EXTRA_TEXT).toString()
+    fun extractData(inAppMessage: InAppMessage, inAppMessageAction: Action) {
+
+        val inAppMessageData: HashMap<String, String> = inAppMessage.data as HashMap<String, String>
+
     }
 }
