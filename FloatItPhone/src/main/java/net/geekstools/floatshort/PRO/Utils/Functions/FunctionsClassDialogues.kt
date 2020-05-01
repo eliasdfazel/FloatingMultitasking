5237789235b412da488bb6df2bd3c4410482f86a
GeeksEmpireNet <Support@GeeksEmpire.net>
2020-05-01 08:19:43 -0800

    [Time] 01-May-2020 | 80:20
    [Description]
    #
    
    [Phone][ChangeLog]
    
    [Watch][ChangeLog]
    
    [Build][ChangeLog]
    # Libraries Updated

--- FloatItPhone/src/main/java/net/geekstools/floatshort/PRO/Utils/Functions/FunctionsClassDialogues.kt
+++ FloatItPhone/src/main/java/net/geekstools/floatshort/PRO/Utils/Functions/FunctionsClassDialogues.kt
@@ -1,8 +1,8 @@
 /*
  * Copyright © 2020 By Geeks Empire.
  *
- * Created by Elias Fazel on 3/24/20 1:15 PM
- * Last modified 3/24/20 10:35 AM
+ * Created by Elias Fazel
+ * Last modified 5/1/20 8:19 AM
  *
  * Licensed Under MIT License.
  * https://opensource.org/licenses/MIT
@@ -22,14 +22,8 @@ import android.view.View
 import android.view.Window
 import android.view.WindowManager
 import android.widget.RelativeLayout
-import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.FirebaseUser
-import com.google.firebase.storage.FirebaseStorage
 import kotlinx.android.synthetic.main.dialogue_message.*
-import net.geekstools.floatshort.PRO.BuildConfig
 import net.geekstools.floatshort.PRO.R
-import net.geekstools.floatshort.PRO.Utils.Functions.FunctionsClassDebug.Companion.PrintDebug
-import java.io.File
 
 class FunctionsClassDialogues (var functionsClassDataActivity: FunctionsClassDataActivity, var functionsClass: FunctionsClass) {
 
@@ -84,27 +78,13 @@ class FunctionsClassDialogues (var functionsClassDataActivity: FunctionsClassDat
         }
 
         if (!functionsClassDataActivity.activity.getFileStreamPath(".Updated").exists()) {
+
             dialog.show()
+
         } else if (functionsClass.appVersionCode(functionsClassDataActivity.activity.packageName) > functionsClass.readFile(".Updated").toInt()) {
+
             dialog.show()
-            if (!BuildConfig.DEBUG && functionsClass.networkConnection()) {
-                FirebaseAuth.getInstance().addAuthStateListener {
-                    val user: FirebaseUser? = it.currentUser
-                    if (user == null) {
-                        functionsClass.savePreference(".UserInformation", "userEmail", null)
-                    } else {
-                        val betaFile = File("/data/data/" + functionsClassDataActivity.activity.packageName + "/shared_prefs/.UserInformation.xml")
-                        val uriBetaFile = Uri.fromFile(betaFile)
-                        val firebaseStorage = FirebaseStorage.getInstance()
 
-                        val storageReference = firebaseStorage.getReference("/Users/" + "API" + functionsClass.returnAPI() + "/" +
-                                functionsClass.readPreference(".UserInformation", "userEmail", null))
-                        val uploadTask = storageReference.putFile(uriBetaFile)
-
-                        uploadTask.addOnFailureListener { exception -> exception.printStackTrace() }.addOnSuccessListener { PrintDebug("Firebase Activities Done Successfully") }
-                    }
-                }
-            }
         }
     }
 
