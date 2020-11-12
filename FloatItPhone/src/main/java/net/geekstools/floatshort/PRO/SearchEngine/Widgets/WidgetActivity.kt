e527cda536aeefd4dd7c9b4b0e8fcadfda6c7c9c
GeeksEmpireNet <Support@GeeksEmpire.net>
2020-11-12 11:42:44 -0900

    [Description] # Optimizing Functionality & UI with Kotlin & androidX Libraries # Refactoring Deprecated API & Functions
    [Phone][ChangeLog]
    
    [Watch][ChangeLog]
    
    [Build][ChangeLog]
    # Libraries Updated

--- FloatItPhone/src/main/java/net/geekstools/floatshort/PRO/SearchEngine/Widgets/WidgetActivity.kt
+++ FloatItPhone/src/main/java/net/geekstools/floatshort/PRO/SearchEngine/Widgets/WidgetActivity.kt
@@ -2,7 +2,7 @@
  * Copyright © 2020 By Geeks Empire.
  *
  * Created by Elias Fazel
- * Last modified 11/12/20 11:33 AM
+ * Last modified 11/12/20 11:42 AM
  *
  * Licensed Under MIT License.
  * https://opensource.org/licenses/MIT
@@ -12,11 +12,16 @@ package net.geekstools.floatshort.PRO.SearchEngine.Widgets
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import net.geekstools.floatshort.PRO.databinding.SearchEngineWidgetActivityBinding
 
 class WidgetActivity : AppCompatActivity() {
 
+    lateinit var searchEngineWidgetActivityBinding: SearchEngineWidgetActivityBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        searchEngineWidgetActivityBinding = SearchEngineWidgetActivityBinding.inflate(layoutInflater)
+        setContentView(searchEngineWidgetActivityBinding.root)
 
 
 
