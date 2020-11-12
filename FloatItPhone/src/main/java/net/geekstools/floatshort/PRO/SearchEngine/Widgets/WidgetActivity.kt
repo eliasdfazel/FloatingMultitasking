08f251d7813e7341ec967f68afc0a759070a17bd
GeeksEmpireNet <Support@GeeksEmpire.net>
2020-11-12 11:49:25 -0900

    [Description] # Optimizing Functionality & UI with Kotlin & androidX Libraries # Refactoring Deprecated API & Functions
    [Phone][ChangeLog]
    
    [Watch][ChangeLog]
    
    [Build][ChangeLog]
    # Libraries Updated

--- FloatItPhone/src/main/java/net/geekstools/floatshort/PRO/SearchEngine/Widgets/WidgetActivity.kt
+++ FloatItPhone/src/main/java/net/geekstools/floatshort/PRO/SearchEngine/Widgets/WidgetActivity.kt
@@ -2,7 +2,7 @@
  * Copyright © 2020 By Geeks Empire.
  *
  * Created by Elias Fazel
- * Last modified 11/12/20 11:42 AM
+ * Last modified 11/12/20 11:47 AM
  *
  * Licensed Under MIT License.
  * https://opensource.org/licenses/MIT
@@ -12,9 +12,19 @@ package net.geekstools.floatshort.PRO.SearchEngine.Widgets
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import net.geekstools.floatshort.PRO.SearchEngine.UI.SearchEngine
+import net.geekstools.floatshort.PRO.Utils.Functions.FileIO
+import net.geekstools.floatshort.PRO.Utils.Functions.FloatingServices
+import net.geekstools.floatshort.PRO.Utils.Functions.FunctionsClassLegacy
+import net.geekstools.floatshort.PRO.Utils.UI.CustomIconManager.LoadCustomIcons
 import net.geekstools.floatshort.PRO.databinding.SearchEngineWidgetActivityBinding
 
 class WidgetActivity : AppCompatActivity() {
+
+    private val firebaseAuth: FirebaseAuth = Firebase.auth
 
     lateinit var searchEngineWidgetActivityBinding: SearchEngineWidgetActivityBinding
 
@@ -23,7 +33,23 @@ class WidgetActivity : AppCompatActivity() {
         searchEngineWidgetActivityBinding = SearchEngineWidgetActivityBinding.inflate(layoutInflater)
         setContentView(searchEngineWidgetActivityBinding.root)
 
+        val functionsClassLegacy = FunctionsClassLegacy(applicationContext)
+        val fileIO = FileIO(applicationContext)
+        val floatingServices = FloatingServices(applicationContext)
+        val loadCustomIcons = LoadCustomIcons(applicationContext, functionsClassLegacy.customIconPackageName())
 
+        /*Search Engine*/
+        SearchEngine(activity = this@WidgetActivity, context = applicationContext,
+                searchEngineViewBinding = searchEngineWidgetActivityBinding.searchEngineViewInclude,
+                functionsClassLegacy = functionsClassLegacy,
+                fileIO = fileIO,
+                floatingServices = floatingServices,
+                customIcons = loadCustomIcons,
+                firebaseAuth = firebaseAuth).apply {
+
+            initializeSearchEngineData()
+        }
+        /*Search Engine*/
 
     }
 
