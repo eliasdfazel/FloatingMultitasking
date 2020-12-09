b29f5ca9b6a117d463d7e533b00a7c6aed37bf5f
GeeksEmpireNet <Support@GeeksEmpire.net>
2020-12-09 01:17:17 -0900

    [Description] # Optimizing Functionality & UI with Kotlin & androidX Libraries # Refactoring Deprecated API & Functions
    [Phone][ChangeLog]
    
    [Watch][ChangeLog]
    
    [Build][ChangeLog]
    # Libraries Updated

--- FloatItPhone/src/main/java/net/geekstools/floatshort/PRO/Shortcuts/ApplicationsViewPhone.kt
+++ FloatItPhone/src/main/java/net/geekstools/floatshort/PRO/Shortcuts/ApplicationsViewPhone.kt
@@ -2,7 +2,7 @@
  * Copyright © 2020 By Geeks Empire.
  *
  * Created by Elias Fazel
- * Last modified 12/6/20 7:16 AM
+ * Last modified 12/9/20 1:07 AM
  *
  * Licensed Under MIT License.
  * https://opensource.org/licenses/MIT
@@ -743,7 +743,11 @@ class ApplicationsViewPhone : AppCompatActivity(),
             swipeGestureListener.onTouchEvent(it)
         }
 
-        return super.dispatchTouchEvent(motionEvent)
+        return if (motionEvent != null) {
+            super.dispatchTouchEvent(motionEvent)
+        } else {
+            false
+        }
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
