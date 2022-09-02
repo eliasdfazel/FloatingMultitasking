922465b441a323ec8fb56468ab42a086405cc2c6
Elias Fazel <elias.fazel@gmail.com>
2022-09-02 03:12:23 -0700

    [Description] ~ Float It for WearOS Removed Due to Removal of Overly Permissions on WearOS
    # Optimizing Functionality & UI with Kotlin & androidX Libraries
    # Refactoring Deprecated API & Functions
    
    [Phone][ChangeLog]
    
    [Watch][ChangeLog]
    
    [Build][ChangeLog]
    # Libraries Updated

--- FloatItPhone/src/main/java/net/geekstools/floatshort/PRO/Utils/RemoteTask/Create/FloatIt.kt
+++ FloatItPhone/src/main/java/net/geekstools/floatshort/PRO/Utils/RemoteTask/Create/FloatIt.kt
@@ -6,6 +6,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.util.TypedValue
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import net.geekstools.floatshort.PRO.SecurityServices.AuthenticationProcess.Fingerprint.AuthenticationFingerprint
 import net.geekstools.floatshort.PRO.SecurityServices.AuthenticationProcess.Utils.AuthenticationCallback
@@ -13,12 +14,17 @@ import net.geekstools.floatshort.PRO.SecurityServices.AuthenticationProcess.Util
 import net.geekstools.floatshort.PRO.SecurityServices.AuthenticationProcess.Utils.SecurityInterfaceHolder
 import net.geekstools.floatshort.PRO.Shortcuts.FloatingServices.Utils.OpenActions
 import net.geekstools.floatshort.PRO.Utils.Functions.FunctionsClassLegacy
+import net.geekstools.floatshort.PRO.Utils.Functions.PreferencesIO
 import net.geekstools.floatshort.PRO.Utils.Functions.PublicVariable
 
 class FloatIt : AppCompatActivity() {
 
     val functionsClassLegacy by lazy {
         FunctionsClassLegacy(applicationContext)
+    }
+
+    val preferencesIO: PreferencesIO by lazy {
+        PreferencesIO(applicationContext)
     }
 
     val securityFunctions by lazy {
@@ -32,7 +38,7 @@ class FloatIt : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        PublicVariable.floatingSizeNumber = functionsClassLegacy.readDefaultPreference("floatingSize", 39)
+        PublicVariable.floatingSizeNumber = preferencesIO.readDefaultPreference("floatingSize", 39)
         PublicVariable.floatingViewsHW = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, PublicVariable.floatingSizeNumber.toFloat(), resources.displayMetrics).toInt()
 
         val aPackageName = intent.getStringExtra("PackageName")!!
@@ -40,48 +46,79 @@ class FloatIt : AppCompatActivity() {
 
         val sharedPreferencesPosition = getSharedPreferences(aClassName, Context.MODE_PRIVATE)
 
-
         val xPosition = sharedPreferencesPosition.getInt("X", 137)
         val yPosition = sharedPreferencesPosition.getInt("Y", 137)
 
-        if (securityFunctions.isAppLocked(aPackageName)) {
+        if (functionsClassLegacy.FreeForm()) {
 
-            SecurityInterfaceHolder.authenticationCallback = object : AuthenticationCallback {
+            if (securityFunctions.isAppLocked(aPackageName)) {
 
-                override fun authenticatedFloatIt(extraInformation: Bundle?) {
-                    super.authenticatedFloatIt(extraInformation)
-                    Log.d(this@FloatIt.javaClass.simpleName, "AuthenticatedFloatingShortcuts")
+                invokeSecurityServices()
 
-                    openActions.startProcess(aPackageName, aClassName, xPosition, yPosition, PublicVariable.floatingViewsHW)
+            } else {
 
-                }
+                openActions.startProcess(aPackageName, aClassName, xPosition, yPosition, PublicVariable.floatingViewsHW)
 
-                override fun failedAuthenticated() {
-                    super.failedAuthenticated()
-                    Log.d(this@FloatIt.javaClass.simpleName, "FailedAuthenticated")
-                }
-
-                override fun invokedPinPassword() {
-                    super.invokedPinPassword()
-                    Log.d(this@FloatIt.javaClass.simpleName, "InvokedPinPassword")
-                }
             }
-
-            startActivity(Intent(applicationContext, AuthenticationFingerprint::class.java).apply {
-                putExtra("PackageName", aPackageName)
-                putExtra("ClassName", aClassName)
-                putExtra("PrimaryColor", functionsClassLegacy.extractDominantColor(functionsClassLegacy.applicationIcon(packageName)))
-                addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-            }, ActivityOptions.makeCustomAnimation(applicationContext, android.R.anim.fade_in, 0).toBundle())
-
 
         } else {
 
-            openActions.startProcess(aPackageName, aClassName, xPosition, yPosition, PublicVariable.floatingViewsHW)
+            Toast.makeText(applicationContext, "Enable Extended Float It In Preferences ⚙️", Toast.LENGTH_LONG).show()
+
+            if (securityFunctions.isAppLocked(aPackageName)) {
+
+                invokeSecurityServices()
+
+            } else {
+
+                openActions.startProcess(aPackageName, aClassName, xPosition, yPosition, PublicVariable.floatingViewsHW)
+
+            }
 
         }
 
         this@FloatIt.finish()
+
+    }
+
+    private fun invokeSecurityServices() {
+
+        val aPackageName = intent.getStringExtra("PackageName")!!
+        val aClassName = intent.getStringExtra("ClassName")!!
+
+        val sharedPreferencesPosition = getSharedPreferences(aClassName, Context.MODE_PRIVATE)
+
+        val xPosition = sharedPreferencesPosition.getInt("X", 137)
+        val yPosition = sharedPreferencesPosition.getInt("Y", 137)
+
+        SecurityInterfaceHolder.authenticationCallback = object : AuthenticationCallback {
+
+            override fun authenticatedFloatIt(extraInformation: Bundle?) {
+                super.authenticatedFloatIt(extraInformation)
+                Log.d(this@FloatIt.javaClass.simpleName, "AuthenticatedFloatingShortcuts")
+
+                openActions.startProcess(aPackageName, aClassName, xPosition, yPosition, PublicVariable.floatingViewsHW)
+
+            }
+
+            override fun failedAuthenticated() {
+                super.failedAuthenticated()
+                Log.d(this@FloatIt.javaClass.simpleName, "FailedAuthenticated")
+            }
+
+            override fun invokedPinPassword() {
+                super.invokedPinPassword()
+                Log.d(this@FloatIt.javaClass.simpleName, "InvokedPinPassword")
+            }
+        }
+
+        startActivity(Intent(applicationContext, AuthenticationFingerprint::class.java).apply {
+            putExtra("PackageName", aPackageName)
+            putExtra("ClassName", aClassName)
+            putExtra("PrimaryColor", functionsClassLegacy.extractDominantColor(functionsClassLegacy.applicationIcon(packageName)))
+            addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+        }, ActivityOptions.makeCustomAnimation(applicationContext, android.R.anim.fade_in, 0).toBundle())
+
     }
 
 }
