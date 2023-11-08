c9712602e830a21bb560c9aceab1b9927191464e
Elias Fazel <elias.fazel@gmail.com>
2023-11-08 03:39:08 -0800

    [Description] # Optimizing Functionality & UI with Kotlin & androidX Libraries # Refactoring Deprecated API & Functions
    [Phone][ChangeLog]
    ~ API 34 Issues Fixed {
    Broadcast Receiver
    }
    
    [Build][ChangeLog]
    # Libraries Updated

--- FloatItPhone/src/main/java/net/geekstools/floatshort/PRO/BindServices.kt
+++ FloatItPhone/src/main/java/net/geekstools/floatshort/PRO/BindServices.kt
@@ -64,7 +64,6 @@ class BindServices : Service() {
         functionsClassLegacy.checkLightDarkTheme()
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
-            println(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> done")
             startForeground(333, functionsClassLegacy.bindServiceNotification(), ServiceInfo.FOREGROUND_SERVICE_TYPE_SPECIAL_USE)
         } else {
             startForeground(333, functionsClassLegacy.bindServiceNotification())
