692411271f6ced497018e676becdb4d3edff7f8a
Elias Fazel <elias.fazel@gmail.com>
2024-01-18 10:08:21 -0500

    [Description] # Optimizing Functionality & UI with Kotlin & androidX Libraries # Refactoring Deprecated API & Functions
    [Phone][ChangeLog]
    ~ Float It Reconstructed
    
    [Build][ChangeLog]
    # Libraries Updated

--- FloatItPhone/src/main/java/net/geekstools/floatshort/PRO/Utils/InteractionObserver/SplitTransparentSingle.kt
+++ FloatItPhone/src/main/java/net/geekstools/floatshort/PRO/Utils/InteractionObserver/SplitTransparentSingle.kt
@@ -11,13 +11,15 @@ package net.geekstools.floatshort.PRO.Utils.InteractionObserver
 
 import android.app.Activity
 import android.content.Intent
-import android.graphics.Color
+import android.os.Build
 import android.os.Bundle
 import android.view.View
+import android.view.WindowInsetsController
 import android.view.WindowManager
 import android.view.accessibility.AccessibilityEvent
 import android.view.accessibility.AccessibilityManager
 import android.widget.Button
+import net.geekstools.floatshort.PRO.R
 import net.geekstools.floatshort.PRO.Utils.Functions.PublicVariable
 
 class SplitTransparentSingle : Activity() {
@@ -27,10 +29,20 @@ class SplitTransparentSingle : Activity() {
 
         val window = window
         window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
-        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
-        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
-        window.statusBarColor = Color.TRANSPARENT
-        getWindow().navigationBarColor = Color.TRANSPARENT
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.insetsController?.setSystemBarsAppearance(WindowInsetsController.APPEARANCE_LIGHT_STATUS_BARS,
+                WindowInsetsController.APPEARANCE_LIGHT_STATUS_BARS)
+        } else {
+            @Suppress("DEPRECATION")
+            window.decorView.systemUiVisibility = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR or View.SYSTEM_UI_FLAG_LIGHT_NAVIGATION_BAR
+            } else {
+                View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
+            }
+
+        }
+        window.statusBarColor = getColor(R.color.dark)
+        getWindow().navigationBarColor = getColor(R.color.dark)
 
         val accessibilityManager = getSystemService(ACCESSIBILITY_SERVICE) as AccessibilityManager
 
